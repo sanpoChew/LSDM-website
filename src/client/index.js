f806670b7690318a78395d95b946170531ded4e6
Joe Bushell <joe@sanpochew.com>
2017-03-07 23:47:30 +0000

    remove analytics from bundle

--- src/client/index.js
+++ src/client/index.js
@@ -20,14 +20,6 @@ hyperform(window);
 smoothscroll.polyfill();
 svg4everybody();
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-}(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga'));
-
-ga('create', 'UA-89685544-1', 'auto');
-ga('send', 'pageview');
-
 formListener(document.querySelectorAll('form'));
 
 const movingButtons = document.querySelectorAll('.moveToHeader');

b6dd19f1f3b027abd3dc2917040002f3d19e20b1
Joseph Bushell <joseph.bushell@oddschecker.com>
2018-01-22 22:13:11 +0000

    linter fixes

--- src/server/routes/testimonials.js
+++ src/server/routes/testimonials.js
@@ -1,14 +1,6 @@
 import Router from 'koa-router';
-import Stripe from 'stripe';
 import log from '../index';
 import directus from '../lib/directus';
-import hubspot from '../lib/hubspot';
-
-function removeMeta(data, fields = []) {
-  return data.map(d =>
-    Object.assign(d, ...fields.map(f => ({ [f]: d[f].data[0][f] }))),
-  );
-}
 
 const courseRouter = new Router()
   .get('/', async (ctx) => {
@@ -16,7 +8,7 @@ const courseRouter = new Router()
       const { data } = await directus.getItems('Testimonials');
       await ctx.render('testimonials', Object.assign(ctx.state, {
         pageTitle: 'Testimonials | London School of Digital Marketing',
-        data
+        data,
       }));
     } catch (err) {
       log.error({ err });
@@ -24,19 +16,17 @@ const courseRouter = new Router()
   })
   .get('/:name', async (ctx) => {
     try {
-        const { data: [ testimonial ] } = await directus.getItems('Testimonials', {
-            filters: { Slug: ctx.params.name },
-        });
+      const { data: [testimonial] } = await directus.getItems('Testimonials', {
+        filters: { Slug: ctx.params.name },
+      });
 
-        console.log(testimonial);
-
-        await ctx.render('testimonial', Object.assign(ctx.state, {
-            pageTitle: `${testimonial.Title} | London School of Digital Marketing`,
-            testimonial,
-        }));
+      await ctx.render('testimonial', Object.assign(ctx.state, {
+        pageTitle: `${testimonial.Title} | London School of Digital Marketing`,
+        testimonial,
+      }));
     } catch (err) {
-        log.error({ err });
+      log.error({ err });
     }
-  })
+  });
 
 export default courseRouter;
